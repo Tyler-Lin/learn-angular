38f655d990b287554dbfe3cafed5b2ef916a1e32
Tyler Lin <ddkc747051@gmail.com>
2023-11-21 15:15:47 +0800

    建立基本的router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'test',
+        loadComponent: () =>
+          import('./test/test.component').then(m => m.TestComponent),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
